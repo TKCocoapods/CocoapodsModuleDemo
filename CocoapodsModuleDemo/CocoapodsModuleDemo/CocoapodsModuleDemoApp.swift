01b8ed21e359ba40fac5623a3f84384ecbb47b9e
ZhengXianda <xianda.zheng@jiamiantech.com>
2022-03-10 11:42:43 +0800

    init Demo

---
+++ CocoapodsModuleDemo/CocoapodsModuleDemo/CocoapodsModuleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoapodsModuleDemoApp.swift
+//  CocoapodsModuleDemo
+//
+//  Created by ZhengXianda on 2022/3/9.
+//
+
+import SwiftUI
+
+@main
+struct CocoapodsModuleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
