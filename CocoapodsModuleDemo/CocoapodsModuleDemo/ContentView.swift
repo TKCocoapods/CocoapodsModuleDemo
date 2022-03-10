d2d12c8b2b86b1567d14eac3aa69d7b70ac8cad3
ZhengXianda <xianda.zheng@jiamiantech.com>
2022-03-10 14:16:48 +0800

    集成 本地库 TKCPSpeaker

--- CocoapodsModuleDemo/CocoapodsModuleDemo/ContentView.swift
+++ CocoapodsModuleDemo/CocoapodsModuleDemo/ContentView.swift
@@ -6,11 +6,31 @@
 //
 
 import SwiftUI
+import TKCPSpeaker
+
+extension View {
+    public func space(_ width: CGFloat? = nil, _ color: Color = Color.blue) -> some View {
+        return self.frame(width: width, height: 50, alignment: .center)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(color, lineWidth: 1)
+            )
+    }
+}
 
 struct ContentView: View {
+    @State private var msg: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(msg).space(300, Color.gray)
+            Button(action: {
+                msg = TKCPSpeaker.intro()
+            }){
+                Text("Say!").space(300)
+            }
+        }
     }
 }
 
